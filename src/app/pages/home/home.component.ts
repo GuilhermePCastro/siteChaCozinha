30f230e872b2ad85fd75f585f3466b0ddc8ba93c
Guilherme Castro <guibkk@gmail.com>
2025-07-10 21:47:02 -0300

    Novos ajustes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,11 @@ import { CarroselComponent } from './../../components/carrosel/carrosel.componen
 import { ComoFuncionaComponent } from "../../components/como-funciona/como-funciona.component";
 import { FotoEstaticaComponent } from "../../components/foto-estatica/foto-estatica.component";
 import { ProdutosComponent } from "../../components/produtos/produtos.component";
+import { FooterComponent } from "../../footer/footer.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CarroselComponent, ComoFuncionaComponent, FotoEstaticaComponent, ProdutosComponent],
+  imports: [HeaderComponent, CarroselComponent, ComoFuncionaComponent, FotoEstaticaComponent, ProdutosComponent, FooterComponent],
 templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
